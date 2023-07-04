f1cfba1bfd5184f19c7cfae27ba6d9341af2fce9
dhampir7 <dhampir7@naver.com>
2023-07-04 21:57:23 +0900

    7/4

--- ConsoleApp1/Chapter4/예제4.13/4.3.2.4.cs
+++ ConsoleApp1/Chapter4/예제4.13/4.3.2.4.cs
@@ -1,12 +1,32 @@
-﻿/*예제 4.3.2.2 GetType*/
+﻿/*예제 4.3.2.4 GetHashCode*/
 
+class Book
+{
+    decimal _isbn;
+
+    public Book(decimal isbn)
+    {
+        _isbn = isbn;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        string txt1 = new string(new char[] { 't', 'e', 'x', 't' });
-        string txt2 = new string(new char[] { 't', 'e', 'x', 't' });
+        short n1 = 256;
+        short n2 = 32750;
+        short n3 = 256;
 
-        Console.WriteLine(txt1.Equals(txt2));   // 출력 결과: True
+        Console.WriteLine(n1.GetHashCode());    // 출력 결과: 16777472
+        Console.WriteLine(n2.GetHashCode());    // 출력 결과: 2146336750
+        Console.WriteLine(n3.GetHashCode());    // 출력 결과: 16777472
+
+        Book book1 = new Book(9788998139018);
+        Book book2 = new Book(9788998139018);
+
+        Console.WriteLine(book1.GetHashCode()); // 출력 결과: 46104728
+                                                // [임의의 값으로 실행할 때마다 달라짐]
+        Console.WriteLine(book2.GetHashCode()); // 출력 결과: 12289376
+                                                // [임의의 값으로 실행할 때마다 달라짐]
     }
 }
---
+++ ConsoleApp1/Chapter4/예제4.13/4.3.2.41.cs
@@ -0,0 +1,22 @@
+﻿/*예제 4.3.2.4 GetHashCode*/
+
+class Book
+{
+    decimal _isbn;
+
+    public Book(decimal isbn)
+    {
+        _isbn = isbn;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n1 = 256;
+        int n2 = 2147483647;
+
+        Console.WriteLine(n1.GetHashCode());    // 출력 결과: 256
+        Console.WriteLine(n2.GetHashCode());    // 출력 결과: 2147483647
+    }
+}
