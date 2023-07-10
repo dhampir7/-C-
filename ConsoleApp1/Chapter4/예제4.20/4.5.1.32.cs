93736d764814480c995a1627a7b75b85564b363a
dhampir7 <dhampir7@naver.com>
2023-07-10 22:39:15 +0900

    7/10

---
+++ ConsoleApp1/Chapter4/예제4.20/4.5.1.32.cs
@@ -0,0 +1,30 @@
+﻿/*4.5.1.3 델리게이트*/
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate void CalcDelegate(int x, int y);
+
+        static void Add(int x, int y) { Console.WriteLine(x + y); }
+        static void Subtract(int x, int y) { Console.WriteLine(x - y); }
+        static void Multiply(int x, int y) { Console.WriteLine(x * y); }
+        static void Divide(int x, int y) { Console.WriteLine(x / y); }
+
+        static void Main(string[] args)
+        {
+            CalcDelegate calc = new CalcDelegate(Add);
+            CalcDelegate subtractCalc = new CalcDelegate(Subtract);
+            CalcDelegate multiplyCalc = new CalcDelegate(Multiply);
+            CalcDelegate divideCalc = new CalcDelegate(Divide);
+
+            calc = CalcDelegate.Combine(calc, subtractCalc) as CalcDelegate;
+            calc = CalcDelegate.Combine(calc, multiplyCalc) as CalcDelegate;
+            calc = CalcDelegate.Combine(calc, divideCalc) as CalcDelegate;
+
+            calc(10, 5);
+        }
+    }
+}
---
+++ ConsoleApp1/Chapter4/예제4.21/Program.cs
@@ -0,0 +1,44 @@
+﻿/*예제 4.18 ToString을 재정의한 Point*/
+
+using System;
+
+delegate int GetResultDelegate();   // int를 반환하고 매개변수가 없는 델리게이트 타입을 정의
+
+class Target
+{
+    public void Do(GetResultDelegate getResult)
+    {
+        Console.WriteLine(getResult()); // 콜백 메서드 호출
+    }
+}
+
+class Source
+{
+    public int GetResult()  // 콜백 용도로 전달될 메서드
+    {
+        return 0;
+    }
+
+    public void Test()
+    {
+        Target target = new Target();
+        target.Do(new GetResultDelegate(this.GetResult));
+    }
+}
+
+class Program
+{
+    // 3개의 매개변수를 받고 void를 반환하는 델리게이트 정의
+    // 매개변수의 타입이 중요할 뿐 매개변수의 이름은 임의로 정할 수 있음.
+    delegate void WorkDelegate(char arg1, int arg2, int arg3);
+    static void Main(string[] args)
+    {
+        Mathematics math = new Mathematics();
+        WorkDelegate work = math.Calculate;
+
+        work('+', 10, 5);
+        work('-', 10, 5);
+        work('*', 10, 5);
+        work('/', 10, 5);
+    }
+}
